7e8cca1a80f9ff824f62b38af0245a72ba895501
0Shraddha <shraddhadongol12345@gmail.com>
2023-06-14 23:25:03 +0545

    add patient feature added

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/PatientController.java
@@ -0,0 +1,69 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Patient;
+import com.luv2code.springboot.thymeleafdemo.service.PatientService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/patients")
+public class PatientController {
+
+private PatientService patientService;
+
+public PatientController(PatientService thePatientService) {
+	patientService = thePatientService;
+}
+
+	// add mapping for "/list"
+
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		//get patient from db
+		List<Patient> thePatients = patientService.findAll();
+		
+		// add to the spring model that can be used in thymeleaf template
+		theModel.addAttribute("patients", thePatients);
+
+		return "patients/list-patients";
+	}
+	
+	
+@GetMapping("/showFormForAdd")
+public String showFormForAdd(Model theModel) {
+	//create model attribute to bind form data
+	Patient thePatient = new Patient();
+	theModel.addAttribute("patient", thePatient);
+	
+	return "patients/AddPatientForm";
+}
+@PostMapping("/save")
+public String savePatinet(@ModelAttribute("patient") Patient thePatient) {
+	
+	//save the patient
+	patientService.save(thePatient);
+	
+	//use a redirect to prevent duplicate submissions
+	return "redirect:/patients/list";
+}
+	
+	
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Patient.java
@@ -0,0 +1,153 @@
+package com.luv2code.springboot.thymeleafdemo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="patient")
+public class Patient {
+
+	// define fields
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="full_name")
+	private String fullName;
+	
+	@Column(name="email")
+	private String email;
+	
+	
+	@Column(name="phone")
+	private String phone;
+	
+		
+	@Column(name="date")
+	private String date;
+	
+	@Column(name="disease")
+	private String disease;
+	
+	
+	// define constructors
+	
+	public Patient() {
+		
+	}
+
+
+	public Patient(int id, String fullName, String email, String phone, String date, String disease) {
+		super();
+		this.id = id;
+		this.fullName = fullName;
+		this.email = email;
+		this.phone = phone;
+		this.date = date;
+		this.disease = disease;
+	}
+
+
+	public Patient(String fullName, String email, String phone, String date, String disease) {
+		super();
+		this.fullName = fullName;
+		this.email = email;
+		this.phone = phone;
+		this.date = date;
+		this.disease = disease;
+	}
+
+	// define getter/setter
+	
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFullName() {
+		return fullName;
+	}
+
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public String getDate() {
+		return date;
+	}
+
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+
+	public String getDisease() {
+		return disease;
+	}
+
+
+	public void setDisease(String disease) {
+		this.disease = disease;
+	}
+
+	// define toString
+
+	@Override
+	public String toString() {
+		return "Patient [id=" + id +
+				", fullName=" + fullName + 
+				", email=" + email + 
+				", phone=" + phone + 
+				", date="+ date + 
+				", disease=" + disease + 
+				"]";
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
