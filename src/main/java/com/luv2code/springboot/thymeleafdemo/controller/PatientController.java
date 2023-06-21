252163165bda901fda798ef1204435f5df6f3aff
0Shraddha <shraddhadongol12345@gmail.com>
2023-06-21 13:56:13 +0545

    pages linked in backend

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/PatientController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/PatientController.java
@@ -2,7 +2,7 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Patient;
 import com.luv2code.springboot.thymeleafdemo.service.PatientService;
-import jakarta.annotation.PostConstruct;
+// import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -47,6 +46,65 @@ public String showFormForAdd(Model theModel) {
 	
 	return "patients/AddPatientForm";
 }
+@GetMapping("/showFormForAddUser")
+public String showFormForAddUser(Model theModel) {
+	//create model attribute to bind form data
+	Patient thePatient = new Patient();
+	theModel.addAttribute("patient", thePatient);
+	
+	return "patients/addPatientFormUser";
+}
+@GetMapping("/showAdminIndex")
+public String showAdminIndex(Model theModel) {
+	//create model attribute to bind form data
+	Patient thePatient = new Patient();
+	theModel.addAttribute("patient", thePatient);
+	
+	return "patients/Admin-index";
+}
+@GetMapping("/showBloodForAdmin")
+public String showBloodForAdmin(Model theModel) {
+	//create model attribute to bind form data
+	Patient thePatient = new Patient();
+	theModel.addAttribute("patient", thePatient);
+	
+	return "patients/admin-blood";
+}
+@GetMapping("/showAmbulanceForAdmin")
+public String showAmbulanceForAdmin(Model theModel) {
+	//create model attribute to bind form data
+	Patient thePatient = new Patient();
+	theModel.addAttribute("patient", thePatient);
+	
+	return "patients/admin-ambulance";
+}
+@GetMapping("/showBlog")
+public String showBlog(Model theModel) {
+	//create model attribute to bind form data
+	Patient thePatient = new Patient();
+	theModel.addAttribute("patient", thePatient);
+	
+	return "patients/blog";
+}
+//@Controller
+//public class MyController {
+//  
+//  @GetMapping("/example")
+//  public String example(Model model) {
+//    // Define the variables and set their values
+//    String option1Value = "Dr.Rajesh Maharjan -12th July,2023 ";
+//    String option2Value = "Dr.Shsir Pradhan -15th July,2023";
+//    String option3Value = "Dr.Sajen Tul -16th July,2023 ";
+//    
+//    // Add the variables to the model
+//    model.addAttribute("option1Value", option1Value);
+//    model.addAttribute("option2Value", option2Value);
+//    model.addAttribute("option3Value", option3Value);
+//    
+//    // Return the name of the Thymeleaf template
+//    return "example-template";
+//  }
+//}
 
 @GetMapping("/showFormForUpdate")
 public String showFormForUpdate(@RequestParam("patientId")int theId, Model theModel) {
@@ -57,8 +115,9 @@ public String showFormForUpdate(@RequestParam("patientId")int theId, Model theMo
 	//send over to our form
 	return "patients/AddPatientForm";
 }
+
 @PostMapping("/save")
-public String savePatinet(@ModelAttribute("patient") Patient thePatient) {
+public String savePatient(@ModelAttribute("patient") Patient thePatient) {
 	
 	//save the patient
 	patientService.save(thePatient);
@@ -66,7 +125,15 @@ public String savePatinet(@ModelAttribute("patient") Patient thePatient) {
 	//use a redirect to prevent duplicate submissions
 	return "redirect:/patients/list";
 }
+@PostMapping("/newForm")
+public String savePatientUser(@ModelAttribute("patient") Patient thePatient) {
 	
+	//save the patient
+	patientService.save(thePatient);
+	
+	//use a redirect to prevent duplicate submissions
+	return "redirect:/patients/showFormForAddUser";
+}
 	
 @GetMapping("/delete")
 public String delete(@RequestParam("patientId")int theId) {
@@ -76,6 +143,7 @@ public String delete(@RequestParam("patientId")int theId) {
 	//redirect to the /patient/list
 	return "redirect:/patients/list";
 }
+
 }
 
 
--- src/main/java/com/luv2code/springboot/thymeleafdemo/dao/PatientRepository.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/PatientRepository.java
@@ -1,5 +1,5 @@
 package com.luv2code.springboot.thymeleafdemo.dao;
-import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Patient;
